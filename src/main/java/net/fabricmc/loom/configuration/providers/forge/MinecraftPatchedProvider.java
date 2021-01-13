f57cfb0db5b01fdeb06612587910e241a8d070ba
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-13 22:27:08 +0200

    Not depend on ForgeGradle and remap using SpecialSource directly
    Co-authored-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/net/fabricmc/loom/configuration/providers/forge/MinecraftPatchedProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/forge/MinecraftPatchedProvider.java
@@ -24,15 +24,20 @@
 
 package net.fabricmc.loom.configuration.providers.forge;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
@@ -42,26 +47,28 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Files;
+import com.google.gson.JsonParser;
 import net.minecraftforge.accesstransformer.TransformerProcessor;
 import net.minecraftforge.binarypatcher.ConsoleTool;
-import net.minecraftforge.gradle.mcp.util.MCPRuntime;
-import net.minecraftforge.gradle.mcp.util.MCPWrapper;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.jetbrains.annotations.Nullable;
+import org.zeroturnaround.zip.ZipUtil;
 
 import net.fabricmc.loom.configuration.DependencyProvider;
 import net.fabricmc.loom.configuration.providers.MinecraftProvider;
 import net.fabricmc.loom.configuration.providers.minecraft.MinecraftMappedProvider;
 import net.fabricmc.loom.util.Checksum;
 import net.fabricmc.loom.util.Constants;
+import net.fabricmc.loom.util.DownloadUtil;
 import net.fabricmc.loom.util.JarUtil;
 import net.fabricmc.loom.util.TinyRemapperMappingsHelper;
 import net.fabricmc.loom.util.function.FsPathConsumer;
+import net.fabricmc.loom.util.srg.SpecialSourceExecutor;
 import net.fabricmc.tinyremapper.OutputConsumerPath;
 import net.fabricmc.tinyremapper.TinyRemapper;
 
@@ -73,9 +80,9 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 	private File minecraftClientPatchedJar;
 	private File minecraftServerPatchedJar;
 	private File minecraftMergedPatchedJar;
-	private File projectAtHash;
+	private Path projectAtHash;
 	@Nullable
-	private File projectAt = null;
+	private Path projectAt = null;
 	private boolean atDirty = false;
 
 	public MinecraftPatchedProvider(Project project) {
@@ -105,25 +112,25 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 	}
 
 	private void initFiles() throws IOException {
-		projectAtHash = new File(getExtension().getProjectPersistentCache(), "at.sha256");
+		projectAtHash = getExtension().getProjectPersistentCache().toPath().resolve("at.sha256");
 
 		SourceSet main = getProject().getConvention().findPlugin(JavaPluginConvention.class).getSourceSets().getByName("main");
 
 		for (File srcDir : main.getResources().getSrcDirs()) {
-			File projectAt = new File(srcDir, "META-INF/accesstransformer.cfg");
+			Path projectAt = srcDir.toPath().resolve("META-INF").resolve("accesstransformer.cfg");
 
-			if (projectAt.exists()) {
+			if (Files.exists(projectAt)) {
 				this.projectAt = projectAt;
 				break;
 			}
 		}
 
-		if (!projectAtHash.exists()) {
+		if (Files.notExists(projectAtHash)) {
 			writeAtHash();
 			atDirty = projectAt != null;
 		} else {
-			byte[] expected = Files.asByteSource(projectAtHash).read();
-			byte[] current = projectAt != null ? Checksum.sha256(projectAt) : Checksum.sha256("");
+			byte[] expected = Files.readAllBytes(projectAtHash);
+			byte[] current = projectAt != null ? Checksum.sha256(projectAt.toFile()) : Checksum.sha256("");
 			boolean mismatched = !Arrays.equals(current, expected);
 
 			if (mismatched) {
@@ -151,9 +158,9 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 	}
 
 	private void writeAtHash() throws IOException {
-		try (FileOutputStream out = new FileOutputStream(projectAtHash)) {
+		try (OutputStream out = Files.newOutputStream(projectAtHash)) {
 			if (projectAt != null) {
-				out.write(Checksum.sha256(projectAt));
+				out.write(Checksum.sha256(projectAt.toFile()));
 			} else {
 				out.write(Checksum.sha256(""));
 			}
@@ -161,27 +168,35 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 	}
 
 	private void createSrgJars(Logger logger) throws Exception {
-		// TODO: FORGE: Get rid of *this*
-		logger.lifecycle(":remapping minecraft (MCP, official -> srg)");
+		logger.lifecycle(":remapping minecraft (SpecialSource, official -> srg)");
 
-		McpConfigProvider volde = getExtension().getMcpConfigProvider();
-		File root = new File(getExtension().getUserCache(), "mcp_root");
-		root.mkdirs();
-		MCPWrapper wrapper = new MCPWrapper(volde.getMcp(), root);
+		McpConfigProvider mcpProvider = getExtension().getMcpConfigProvider();
+		MinecraftProvider minecraftProvider = getExtension().getMinecraftProvider();
 
-		// Client
-		{
-			MCPRuntime runtime = wrapper.getRuntime(getProject(), "client");
-			File output = runtime.execute(logger, "rename");
-			Files.copy(output, minecraftClientSrgJar);
+		String[] mappingsPath = {null};
+		Path[] tmpSrg = {null};
+
+		if (!ZipUtil.handle(mcpProvider.getMcp(), "config.json", (in, zipEntry) -> {
+			mappingsPath[0] = new JsonParser().parse(new InputStreamReader(in)).getAsJsonObject().get("data").getAsJsonObject().get("mappings").getAsString();
+		})) {
+			throw new IllegalStateException("Failed to find 'config.json' in " + mcpProvider.getMcp().getAbsolutePath() + "!");
 		}
 
-		// Server
-		{
-			MCPRuntime runtime = wrapper.getRuntime(getProject(), "server");
-			File output = runtime.execute(logger, "rename");
-			Files.copy(output, minecraftServerSrgJar);
+		if (!ZipUtil.handle(mcpProvider.getMcp(), mappingsPath[0], (in, zipEntry) -> {
+			tmpSrg[0] = Files.createTempFile(null, null);
+
+			try (BufferedWriter writer = Files.newBufferedWriter(tmpSrg[0])) {
+				IOUtils.copy(in, writer, StandardCharsets.UTF_8);
+			}
+		})) {
+			throw new IllegalStateException("Failed to find mappings '" + mappingsPath[0] + "' in " + mcpProvider.getMcp().getAbsolutePath() + "!");
 		}
+
+		File specialSourceJar = new File(getExtension().getUserCache(), "SpecialSource-1.8.3-shaded.jar");
+		DownloadUtil.downloadIfChanged(new URL("https://repo1.maven.org/maven2/net/md-5/SpecialSource/1.8.3/SpecialSource-1.8.3-shaded.jar"), specialSourceJar, getProject().getLogger(), true);
+
+		Files.copy(SpecialSourceExecutor.produceSrgJar(getProject(), specialSourceJar, minecraftProvider.minecraftClientJar.toPath(), tmpSrg[0]), minecraftClientSrgJar.toPath());
+		Files.copy(SpecialSourceExecutor.produceSrgJar(getProject(), specialSourceJar, minecraftProvider.minecraftServerJar.toPath(), tmpSrg[0]), minecraftServerSrgJar.toPath());
 	}
 
 	private void injectForgeClasses(Logger logger) throws IOException {
@@ -212,7 +227,7 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 
 			File atJar = File.createTempFile("at" + side, ".jar");
 			File at = File.createTempFile("at" + side, ".cfg");
-			Files.copy(target, atJar);
+			FileUtils.copyFile(target, atJar);
 			JarUtil.extractFile(atJar, "META-INF/accesstransformer.cfg", at);
 			String[] args = new String[] {
 					"--inJar", atJar.getAbsolutePath(),
@@ -223,7 +238,7 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 			if (projectAt != null) {
 				args = Arrays.copyOf(args, args.length + 2);
 				args[args.length - 2] = "--atFile";
-				args[args.length - 1] = projectAt.getAbsolutePath();
+				args[args.length - 1] = projectAt.toAbsolutePath().toString();
 			}
 
 			TransformerProcessor.main(args);
@@ -279,7 +294,7 @@ public class MinecraftPatchedProvider extends DependencyProvider {
 
 	private void mergeJars(Logger logger) throws IOException {
 		// FIXME: Hack here: There are no server-only classes so we can just copy the client JAR.
-		Files.copy(minecraftClientPatchedJar, minecraftMergedPatchedJar);
+		com.google.common.io.Files.copy(minecraftClientPatchedJar, minecraftMergedPatchedJar);
 
 		logger.lifecycle(":copying resources");
 
