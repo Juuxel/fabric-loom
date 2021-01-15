2ff0ea0bd8354d315813ae8aef6b3faf1cfa8cbf
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-01-15 21:50:13 +0200

    Some JarUtil docs

--- src/main/java/net/fabricmc/loom/util/JarUtil.java
+++ src/main/java/net/fabricmc/loom/util/JarUtil.java
@@ -44,6 +44,14 @@ import com.google.common.collect.ImmutableMap;
  * @author Juuz
  */
 public final class JarUtil {
+	/**
+	 * Creates a new JAR file system. Remember to close it!
+	 *
+	 * @param jar    the JAR path
+	 * @param create whether to create the JAR file
+	 * @return the created file system
+	 * @throws IOException if creating the file system threw an {@link IOException}
+	 */
 	public static FileSystem fs(Path jar, boolean create) throws IOException {
 		return FileSystems.newFileSystem(URI.create("jar:" + jar.toUri()), ImmutableMap.of("create", create));
 	}
@@ -56,6 +64,13 @@ public final class JarUtil {
 		extract(jar, ImmutableMap.of(filePath, target));
 	}
 
+	/**
+	 * Extracts files from a JAR.
+	 *
+	 * @param jar   the JAR file
+	 * @param paths A map of paths in JAR to paths outside the JAR. The keys will be copied to the values.
+	 * @throws IOException if an {@link IOException} occurred while extracting
+	 */
 	public static void extract(Path jar, Map<String, Path> paths) throws IOException {
 		try (FileSystem fs = fs(jar, false)) {
 			for (Map.Entry<String, Path> entry : paths.entrySet()) {
@@ -65,6 +80,13 @@ public final class JarUtil {
 		}
 	}
 
+	/**
+	 * Writes the specified lines to a file. Existing files with the same path will be deleted.
+	 *
+	 * @param path  the target path
+	 * @param lines the lines of the file
+	 * @throws IOException if an {@link IOException} was thrown when writing
+	 */
 	public static void write(Path path, List<String> lines) throws IOException {
 		Files.deleteIfExists(path);
 
