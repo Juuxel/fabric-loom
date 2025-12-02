303e21cf2c7fe5599e48b5e0c7756ca6d08b9660
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-12-02 11:44:37 +0200

    MPP: Don't put MC resources in the MC classes jar

--- src/main/java/dev/architectury/loom/forge/minecraft/MinecraftPatchedProvider.java
+++ src/main/java/dev/architectury/loom/forge/minecraft/MinecraftPatchedProvider.java
@@ -102,7 +102,7 @@ import net.fabricmc.tinyremapper.extension.mixin.MixinExtension;
 
 public class MinecraftPatchedProvider {
 	private static final String LOOM_PATCH_VERSION_KEY = "Loom-Patch-Version";
-	private static final String CURRENT_LOOM_PATCH_VERSION = "9";
+	private static final String CURRENT_LOOM_PATCH_VERSION = "10";
 	private static final String NAME_MAPPING_SERVICE_PATH = "/inject/META-INF/services/cpw.mods.modlauncher.api.INameMappingService";
 
 	private static final String NEOFORGE_MANUAL_CLEAR_JAR_CREATION_VERSION = "21.10.57-beta";
@@ -477,7 +477,6 @@ public class MinecraftPatchedProvider {
 		TinyRemapper remapper = buildRemapper(serviceFactory, mcInput);
 
 		try (OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(mcOutput).build()) {
-			outputConsumer.addNonClassFiles(mcInput);
 			outputConsumer.addNonClassFiles(forgeJar, NonClassCopyMode.FIX_META_INF, remapper);
 
 			InputTag mcTag = remapper.createInputTag();
